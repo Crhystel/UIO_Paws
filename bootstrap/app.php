6911ae52374e2ac6fd73cfdd261713fcb0df023c
Crhystel <crhystelvelasco213@gmail.com>
2025-11-13 18:29:30 -0500

    trying secure connections

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
             'is.superadmin' => \App\Http\Middleware\IsSuperAdminMiddleware::class,
             'guest' => \Illuminate\Auth\Middleware\RedirectIfAuthenticated::class,
         ]);
+        $middleware->trustProxies(
+            proxies: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_AWS_ELB
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
