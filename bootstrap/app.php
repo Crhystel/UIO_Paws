e2525638d728876fe2464b69ab2032d5bd698260
Crhystel <crhystelvelasco213@gmail.com>
2025-11-13 18:37:14 -0500

    a

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,8 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'guest' => \Illuminate\Auth\Middleware\RedirectIfAuthenticated::class,
         ]);
         $middleware->trustProxies(
-            proxies: '*',
-            headers: Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_AWS_ELB
+            '*',
+            Request::HEADER_X_FORWARDED_ALL
         );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
