7ba8b1c4dd791aac219762d2742784210ce3e797
Mauro0407 <130101420+Mauro0407@users.noreply.github.com>
2025-12-04 19:36:04 -0500

    Dashboard changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <h1>Bienvenido a tu panel, {{ Session::get('user_name') }}!</h1>
-    <p>Desde aquí podrás gestionar tus adopciones, voluntariado y donaciones.</p>
+    <p>Desde aquí podrás gestionar tus adopciones, voluntariado, donaciones y lo necesario.</p>
 
     @if(session('error'))
         <div class="alert alert-danger">{{ session('error') }}</div>
